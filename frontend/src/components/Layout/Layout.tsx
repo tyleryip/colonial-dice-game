886f3135a6f9be215a5bdbde2794160b01bdc6a9
Tyler Yip <tyler.yip@arcurve.com>
2025-04-21 13:33:52 -0600

    feat: install react-dice-roll and react-hexgrid packages

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Layout() {
-    return(<div className="bg-slate-200 h-dvh grid">
-
+    return (<div className="bg-white h-dvh grid place-items-center">
     </div>)
 }
