5289f4e3e6f253f90b569092a8519db019dc8958
Tyler Yip <tyler.yip1@ucalgary.ca>
2025-05-13 20:28:07 -0600

    fix: Navbar imports causing build issues

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,42 +1,48 @@
-import dice_icon from "../../assets/buttons/dice-icon.svg"
-import NavbarToggle from 'react-bootstrap/NavbarToggle'
-import StyledNavbarCollapse from './styles/StyledNavbarCollapse'
-import StyledNavbarText from './styles/StyledNavbarText'
-import HowToPlayModal from '../Modals/HowToPlayModal/HowToPlayModal'
-import { useState } from 'react'
-import StyledNavbarButton from './NavbarButton/styles/StyledNavbarButton'
-import StyledNavbarBrandIcon from './styles/StyledNavbarBrandIcon'
-import StyledNavbarBrand from "./styles/StyledNavbarBrand"
-import StyledNavbar from "./styles/StyledNavbar"
-import SettingsModal from "../Modals/SettingsModal/SettingsModal"
+import dice_icon from "../../assets/buttons/dice-icon.svg";
+import NavbarToggle from "react-bootstrap/NavbarToggle";
+import StyledNavbarCollapse from "./styles/StyledNavbarCollapse";
+import StyledNavbarText from "./styles/StyledNavbarText";
+import HowToPlayModal from "../Modals/HowToPlayModal/HowToPlayModal";
+import { useState } from "react";
+import StyledNavbarButton from "./NavbarButton/styles/StyledNavbarButton";
+import SettingsModal from "../Modals/SettingsModal/SettingsModal";
+import StyledNavbar from "./styles/StyledNavBar";
+import StyledNavbarBrand from "./styles/StyledNavBarBrand";
+import StyledNavbarBrandIcon from "./styles/StyledNavBarBrandIcon";
 
 const Navbar = () => {
-    const [showHowToPlayModal, setShowHowToPlayModal] = useState(false)
-    const [showSettingsModal, setShowSettingsModal] = useState(false)
+  const [showHowToPlayModal, setShowHowToPlayModal] = useState(false);
+  const [showSettingsModal, setShowSettingsModal] = useState(false);
 
-    return (
-        <StyledNavbar expand="sm">
-            <StyledNavbarBrand href={"/"}>
-                <StyledNavbarBrandIcon src={dice_icon} />
-                {"Colonial Dice Game"}
-            </StyledNavbarBrand>
-            <NavbarToggle />
-            <StyledNavbarCollapse>
-                <StyledNavbarText>
-                    <StyledNavbarButton onClick={() => setShowHowToPlayModal(true)}>
-                        {"How to Play"}
-                    </StyledNavbarButton>
-                    <HowToPlayModal show={showHowToPlayModal} onHide={() => setShowHowToPlayModal(false)} />
-                </StyledNavbarText>
-                <StyledNavbarText >
-                    <StyledNavbarButton onClick={() => setShowSettingsModal(true)}>
-                        {"Settings"}
-                    </StyledNavbarButton>
-                    <SettingsModal show={showSettingsModal} onHide={() => setShowSettingsModal(false)} />
-                </StyledNavbarText>
-            </StyledNavbarCollapse>
-        </StyledNavbar>
-    )
-}
+  return (
+    <StyledNavbar expand="sm">
+      <StyledNavbarBrand href={"/"}>
+        <StyledNavbarBrandIcon src={dice_icon} />
+        {"Colonial Dice Game"}
+      </StyledNavbarBrand>
+      <NavbarToggle />
+      <StyledNavbarCollapse>
+        <StyledNavbarText>
+          <StyledNavbarButton onClick={() => setShowHowToPlayModal(true)}>
+            {"How to Play"}
+          </StyledNavbarButton>
+          <HowToPlayModal
+            show={showHowToPlayModal}
+            onHide={() => setShowHowToPlayModal(false)}
+          />
+        </StyledNavbarText>
+        <StyledNavbarText>
+          <StyledNavbarButton onClick={() => setShowSettingsModal(true)}>
+            {"Settings"}
+          </StyledNavbarButton>
+          <SettingsModal
+            show={showSettingsModal}
+            onHide={() => setShowSettingsModal(false)}
+          />
+        </StyledNavbarText>
+      </StyledNavbarCollapse>
+    </StyledNavbar>
+  );
+};
 
-export default Navbar
+export default Navbar;
