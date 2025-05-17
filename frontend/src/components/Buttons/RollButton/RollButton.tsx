e4cebb6cb1991a8ec9127252e2bac01056bb6a1d
Tyler Yip <tyler.yip1@ucalgary.ca>
2025-05-16 21:56:12 -0600

    feat: swap out roll button with React component instead of img

--- frontend/src/components/Buttons/RollButton/RollButton.tsx
+++ frontend/src/components/Buttons/RollButton/RollButton.tsx
@@ -1,70 +1,65 @@
-import StyledRollButton from './styles/StyledRollButton'
-import dice_icon from "/assets/buttons/dice-icon.svg"
-import StyledRollButtonIcon from './styles/StyledRollButtonIcon'
-import { useAppDispatch, useAppSelector } from '../../../store/hooks'
-import { selectIsGamePhaseBuilding, setGamePhase } from '../../../store/slices/gameSlice'
-import { GamePhase } from '../../../constants/enumerations'
-import { rollDice } from '../../../store/slices/diceSlice'
+import StyledRollButton from "./styles/StyledRollButton";
+import { useAppDispatch, useAppSelector } from "../../../store/hooks";
+import {
+  selectIsGamePhaseBuilding,
+  setGamePhase,
+} from "../../../store/slices/gameSlice";
+import { GamePhase } from "../../../constants/enumerations";
+import { rollDice } from "../../../store/slices/diceSlice";
+import { DiceIcon } from "../../../assets/buttons/DiceIcon";
 
 interface RollButtonProps {
-    disabled?: boolean
-    rollCount: number
-    rollDurationMilliseconds: number
-    setRolling: (rolling: boolean) => void
+  disabled?: boolean;
+  rollCount: number;
+  rollDurationMilliseconds: number;
+  setRolling: (rolling: boolean) => void;
 }
 
 const RollButton = (props: RollButtonProps) => {
-    // Props and constants
+  // Props and constants
 
-    const disabled = props.disabled
-    const rollCount = props.rollCount
-    const setRolling = props.setRolling
-    const rollDurationMilliseconds = props.rollDurationMilliseconds
+  const disabled = props.disabled;
+  const rollCount = props.rollCount;
+  const setRolling = props.setRolling;
+  const rollDurationMilliseconds = props.rollDurationMilliseconds;
 
-    // Dispatch
+  // Dispatch
 
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
-    // Selectors
-    const gamePhaseBuilding = useAppSelector(state => selectIsGamePhaseBuilding(state))
+  // Selectors
+  const gamePhaseBuilding = useAppSelector((state) =>
+    selectIsGamePhaseBuilding(state)
+  );
 
-    // Conditional rendering
+  // Conditional rendering
 
-    const tooltip = rollCount === 2
-        ? "1 roll left"
-        : `${3 - rollCount} rolls left`
+  const tooltip =
+    rollCount === 2 ? "1 roll left" : `${3 - rollCount} rolls left`;
 
-    // Event handlers
+  // Event handlers
 
-    function handleClick() {
-        if (gamePhaseBuilding) {
-            dispatch(setGamePhase(GamePhase.Rolling))
-        }
-
-        setTimeout(() => {
-            // Cannot extract and move to roll button because we need to setRolling for the animation
-            setRolling(false);
-
-        }, rollDurationMilliseconds);
-
-        setRolling(true);
-        dispatch(rollDice());
+  function handleClick() {
+    if (gamePhaseBuilding) {
+      dispatch(setGamePhase(GamePhase.Rolling));
     }
 
-    return (
-        <StyledRollButton
-            title={tooltip}
-            disabled={disabled}
-            onClick={handleClick}>
-            {Array.from({ length: 3 }, (_, key) => (
-                <StyledRollButtonIcon
-                    key={key}
-                    src={dice_icon}
-                    alt="Dice"
-                    $used={rollCount > key} />
-            ))}
-        </StyledRollButton>
-    )
-}
+    setTimeout(() => {
+      // Cannot extract and move to roll button because we need to setRolling for the animation
+      setRolling(false);
+    }, rollDurationMilliseconds);
 
-export default RollButton
+    setRolling(true);
+    dispatch(rollDice());
+  }
+
+  return (
+    <StyledRollButton title={tooltip} disabled={disabled} onClick={handleClick}>
+      {Array.from({ length: 3 }, (_, key) => (
+        <DiceIcon key={key} opacity={rollCount > key ? 30 : 100} width={24} />
+      ))}
+    </StyledRollButton>
+  );
+};
+
+export default RollButton;
