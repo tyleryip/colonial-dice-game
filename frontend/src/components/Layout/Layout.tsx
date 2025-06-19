9711e3c700a8401e48159608940d152620666cc8
Tyler Yip <tyler.yip@arcurve.com>
2025-06-19 16:03:56 -0600

    feat: move the Navbar from the layout to the app

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,12 +1,10 @@
 import GameBoard from "../GameBoard/GameBoard";
-import Navbar from "../Navbar/Navbar";
 import ResourceDiceContainer from "../ResourceDiceContainer/ResourceDiceContainer";
 import Scoreboard from "../Scoreboard/Scoreboard";
 import StyledLayout from "./styles/StyledLayout";
 
 export default function Layout() {
     return (<StyledLayout>
-        <Navbar />
         <Scoreboard />
         <GameBoard />
         <ResourceDiceContainer />
