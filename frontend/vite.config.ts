7cd823f30ca6dbc93cb44d784c2ae10a30cbd827
Tyler Yip <tyler.yip@arcurve.com>
2025-08-25 14:10:02 -0600

    feat: add roll up license plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,14 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createViteLicensePlugin } from 'rollup-license-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createViteLicensePlugin()
+  ],
   define: {
     '__APP_VERSION__': JSON.stringify(process.env.npm_package_version)
   },
